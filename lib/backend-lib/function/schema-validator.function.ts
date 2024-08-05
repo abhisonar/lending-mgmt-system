fc7895e4b538dc8bc6590402fa827b454bfd84d0
Abhishek Sonar <abhisonar4121@gmail.com>
2024-08-06 01:14:42 +0530

    schema-validator.function changes

--- lib/backend-lib/function/schema-validator.function.ts
+++ lib/backend-lib/function/schema-validator.function.ts
@@ -1,17 +1,17 @@
-import { ZodError, ZodObject } from 'zod';
-import { SchemaValidatorType } from '../type';
-import { BaseRequestModel, BaseResponseModel } from '../base/base.http';
-import { baseErrorResponse } from './base/reponse.function';
+import {ZodError, ZodObject} from 'zod';
+import {SchemaValidatorType} from '../type';
+import {BaseRequestModel, BaseResponseModel} from '../base/base.http';
+import {baseErrorResponse} from './base/reponse.function';
 
 export function schemaValidatorMiddleware<Z, Q, S>(schema: ZodObject<SchemaValidatorType<Z>>) {
-  return (req: BaseRequestModel<Q>, res: BaseResponseModel<S>) => {
+  return (req: BaseRequestModel, res: BaseResponseModel<S>) => {
     try {
       schema.parse(req.body);
     } catch (error) {
       if (error instanceof ZodError) {
-        baseErrorResponse(res, 'HTTP_STATUS_UNPROCESSABLE_ENTITY', error);
+        baseErrorResponse('HTTP_STATUS_UNPROCESSABLE_ENTITY', error);
       } else {
-        baseErrorResponse(res, 'HTTP_STATUS_INTERNAL_SERVER_ERROR', error);
+        baseErrorResponse('HTTP_STATUS_INTERNAL_SERVER_ERROR', error);
       }
     }
   };

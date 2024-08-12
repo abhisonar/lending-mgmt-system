a15bb0af64a902c3f25ecb63fca8f3f4bae4265e
Abhishek Sonar <abhisonar4121@gmail.com>
2024-08-13 01:54:38 +0530

    added default route to next.config.mjs for /

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/',
+        destination: '/listing',
+        permanent: false,
+      },
+      {
         source: '/listing',
         destination: '/listing/owner',
         permanent: false,
